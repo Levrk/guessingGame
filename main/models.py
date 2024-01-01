a2e03c563405f472d47de5e79478be37a3f4f7b1
Levrk <levrolandkalb@gmail.com>
2023-12-31 19:31:07 -0500

    defined game model

--- main/models.py
+++ main/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Game(models.Model):
+
+    title = models.CharField(max_length=200)
+    year = models.IntegerField()
+    publisher = models.CharField(max_length=200)
+    platform = models.CharField(max_length=400)
+
+    hint = models.CharField(max_length=200)
+    
+    rating = models.IntegerField()
+    series = models.IntegerField(null=True) #null if not in series otherwise the installment number
+    genre1 = models.CharField(max_length=100)
+    genre2 = models.CharField(max_length=100)
+    setting = models.CharField(max_length=100)
+    multiplayer = models.CharField(max_length=200)
+    playtime = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.title} ({self.year}) by {self.publisher}"
